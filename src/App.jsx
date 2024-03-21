e7750bd56f2f25468f7ad62eb02dce4348a75e4d
shashank2907908 <shashanknalogal@gmail.com>
2024-03-21 23:40:46 +0530

    TabButton commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
-import { CORE_CONCEPTS } from "./data"
-
-
+import TabButton from "./components/TabButton";
+import { CORE_CONCEPTS } from "./data";
 
 function App() {
   return (
@@ -17,6 +16,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton> Components </TabButton>
+            <TabButton> JSX </TabButton>
+            <TabButton> Props </TabButton>
+            <TabButton> State </TabButton>
+          </menu>
         </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+export default function TabButton(props) {
+  function handleClick() {
+    console.log("Hello");
+  }
+  return (
+    <li>
+      <button onClick={handleClick}>{props.children}</button>
+    </li>
+  );
+}
